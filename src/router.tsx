ab0809da487a65120d4727226996633bc0eaae5f
Davi Nicacio <daviniccacio@gmail.com>
2026-09-09 17:25:29 -0300

    feat: configura deploy estatico bem-sucedido para github pages

--- src/router.tsx
+++ src/router.tsx
@@ -10,6 +10,8 @@ export const getRouter = () => {
     context: { queryClient },
     scrollRestoration: true,
     defaultPreloadStaleTime: 0,
+    // Caminho hardcoded para o SSR não se perder:
+    basepath: '/davinicacio-dev-showcase/',
   });
 
   return router;
